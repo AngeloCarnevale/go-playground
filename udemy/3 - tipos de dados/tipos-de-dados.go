3b7af8174022c955f55da09914f622291889c72c
AngeloCarnevale <angelo.carnevale@hotmail.com>
2023-12-07 13:28:21 -0300

    finalizando a aula 3 sobre tipos de dados

--- udemy/3 - tipos de dados/tipos-de-dados.go
+++ udemy/3 - tipos de dados/tipos-de-dados.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // tipos de inteiros: int8, int16, int32, int64
 
@@ -21,6 +24,28 @@ func main() {
 	var numero4 byte = 123
 	fmt.Println(numero4)
 
-	var numeroReal1 float32 = 123.45t
+	var numeroReal1 float32 = 1299999999993.45
+	fmt.Println(numeroReal1)
 
+	numeroReal3 :=  12345.67
+	fmt.Println(numeroReal3)
+
+	// STRINGS
+	var str string = "Texto"
+	fmt.Println(str)
+
+	str2 := "Texto 2"
+	fmt.Println(str2)
+
+	char := 'B'
+	fmt.Println(char)
+
+	var booleano1 bool = false
+
+	fmt.Println(booleano1)
+
+
+	var erro error = errors.New("Erro interno")
+	fmt.Println(erro)
+	
 }
