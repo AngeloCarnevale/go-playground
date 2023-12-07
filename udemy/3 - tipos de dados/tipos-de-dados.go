5cd38ae983ab628de6fe24661d39bbacbe26d5bc
AngeloCarnevale <angelo.carnevale@hotmail.com>
2023-12-07 09:16:02 -0300

    aula 3: tipos de variáveis

---
+++ udemy/3 - tipos de dados/tipos-de-dados.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+// tipos de inteiros: int8, int16, int32, int64
+
+func main() {
+	var number int = -100000000
+	fmt.Println(number)
+
+	var numero2 uint32 = 10000
+
+	fmt.Println(numero2)
+
+	// alias
+	// INT32 = RUNE
+	var numero3 rune = 123456
+	fmt.Println(numero3)
+
+	// BYTE = UINT8
+	var numero4 byte = 123
+	fmt.Println(numero4)
+
+	var numeroReal1 float32 = 123.45t
+
+}
