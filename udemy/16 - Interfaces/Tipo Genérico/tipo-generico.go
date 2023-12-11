3e9c7551216fa02bb8eab1f7450456d22e20c4d8
AngeloCarnevale <angelo.carnevale@hotmail.com>
2023-12-11 10:13:00 -0300

    aula 17.2: Finalizando aplicação de linha de comando

--- udemy/16 - Interfaces/Tipo Genérico/tipo-generico.go
+++ udemy/16 - Interfaces/Tipo Genérico/tipo-generico.go
@@ -10,12 +10,12 @@ func main() {
 	generica("String")
 	generica(1)
 	generica(true)
-	
-	mapa := map[interface{}]interface{} {
-		1: "String",
+
+	mapa := map[interface{}]interface{}{
+		1:            "String",
 		float32(100): true,
-		"String": "String",
-		true: float32(12),
+		"String":     "String",
+		true:         float32(12),
 	}
 
 	fmt.Println(mapa)
--- udemy/17 - Aplicação de Linha de Comando/app/app.go
+++ udemy/17 - Aplicação de Linha de Comando/app/app.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"net"
-
 	"github.com/urfave/cli"
 )
 
@@ -14,17 +13,25 @@ func Gerar() *cli.App {
 	app.Name = "Aplicação de Linha de comando"
 	app.Usage = "Busca IPs e Nomes de Servidor na internet"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name: "host",
+			Value: "devbook.com.br",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
 			Name: "ip",
 			Usage: "Busca IPs de endereços na internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name: "host",
-					Value: "devbook.com.br",
-				},
-			},
+			Flags: flags,
 			Action: buscarIps,
+		},
+		{
+			Name: "servidores",
+			Usage: "Busca o nome dos servidores na internet",
+			Flags: flags,
+			Action: buscarServidores,
 		},
 	}
 	return app
@@ -40,5 +47,17 @@ func buscarIps(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func buscarServidores(c *cli.Context) {
+	host := c.String("host")
+	servidores, erro := net.LookupNS(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, servidor := range servidores {
+		fmt.Println(servidor.Host)
 	}
 }
